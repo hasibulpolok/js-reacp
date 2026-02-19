b1bf91ccbe04da39b3f3952b3a79781bada58040
hasibulpolok <hasibulpolok.bdn@gmail.com>
2026-02-19 09:53:10 +0600

    with arrow function eve odd number

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 // with function 
 
-function EvenOdd (){
-if (num % 2 === 0){
- console.log("Even Number")}
+function EvenOdd() {
+    if (num % 2 === 0) {
+        console.log("Even Number")
+    }
 
- else{
-console.log("Odd Number")
- }
+    else {
+        console.log("Odd Number")
+    }
 }
+
+// with arrow function
+
+const EvenOdd = (num) => {
+    return num % 2 === 0 ? "EvenNumber" : "OddNumber";
+};
+const eoresult = EvenOdd(7);
+console.log(eoresult); 
